4c31db39a743846a6630c3a71acb6c299eefe9b7
Jakub Debosz <jakub.debosz@gmail.com>
2019-10-02 01:46:15 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -0,0 +1,18 @@
+package pl.coderslab.charity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String homeAction(Model model){
+        log.debug("Tańczę w kontrolerze");
+        return "index";
+    }
+}
